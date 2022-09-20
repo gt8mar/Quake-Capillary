51f0e2ad1dd9de2e18d762186d682b6cd670f328
Marcus Forst <35577361+gt8mar@users.noreply.github.com>
2022-09-20 15:39:31 -0700

    Update enfield_pressure_firmware.ino

--- Arduino_firmware/enfield_pressure_firmware.ino
+++ Arduino_firmware/enfield_pressure_firmware.ino
@@ -1,68 +1,96 @@
+// Filename: pressureRegulator.ino
+// Hardware: teensy 4.1
+// ------------------------------------------------------------------------
+// This file controls an Enfield pressure regulator with air input of 2psi.
+// The pressure range is therefore 0 to 2 psi. 
+
 
 const int pwm = 2 ;         //initializing pin 2 as ‘pwm’ variable
-const int buttonPin1 = 3;    // initializing pin 3 as a button variable
-const int buttonPin2 = 4;   //initializing pin 4 as a button variable
+const int buttonPin1 = 4;    // initializing pin 4 as a button variable
+const int buttonPin2 = 5;   //initializing pin 5 as a button variable
+const int LEDPin = 13;
+const int thorlabsLEDPin = 7; 
 
+int buttonState1 = 0;
+int buttonState2 = 0;
 
 void setup()
 {
     pinMode(pwm,OUTPUT) ;     //Set pin 2 as output
     pinMode(buttonPin1, INPUT);  //Set pin 3 as input
     pinMode(buttonPin2, INPUT);  //Set pin 4 as input
+    pinMode(LEDPin, OUTPUT); 
+    pinMode(thorlabsLEDPin, OUTPUT);
+    
 }
 
 
 
 void loop()
 {
-  // read the state of the button
+  // read the state of the button    
     buttonState1 = digitalRead(buttonPin1);
     buttonState2 = digitalRead(buttonPin2);
 
     if (buttonState1 == HIGH && buttonState2 == LOW){
-    
+      digitalWrite(LEDPin, HIGH);
+      
       delay(1000);
       
-      analogWrite(pwm,6) ;         //setting pwm to increase PSI from 0 to 1, incrementing by 0.1 
-      delay(1000) ;             //delay of 5s
+      analogWrite(pwm,127) ;         //setting pwm to increase PSI from 1 to 1.6, incrementing by 0.1 
+      digitalWrite(thorlabsLEDPin, LOW);
+      delay(100);
+            digitalWrite(thorlabsLEDPin, HIGH);
+      delay(1000) ;             //delay of 1s
   
-      analogWrite(pwm,19) ; 
+      analogWrite(pwm,153) ;    // 1.2 psi
+      digitalWrite(thorlabsLEDPin, LOW);
+      delay(100);
+                  digitalWrite(thorlabsLEDPin, HIGH);
       delay(1000) ; 
      
-      analogWrite(pwm,32) ;     // 0.5 psi
+      analogWrite(pwm,178) ;     // 1.4 psi
+      digitalWrite(thorlabsLEDPin, LOW);
+      delay(100);
+                  digitalWrite(thorlabsLEDPin, HIGH);
       delay(1000) ; 
   
-      analogWrite(pwm,45) ; 
+      analogWrite(pwm,203) ;     // 1.6 psi
+            digitalWrite(thorlabsLEDPin, LOW);
+            delay(100);
+                        digitalWrite(thorlabsLEDPin, HIGH);
       delay(1000) ;
   
-      analogWrite(pwm,57) ; 
+      analogWrite(pwm,178) ;      //1.4 psi
+            digitalWrite(thorlabsLEDPin, LOW);
+            delay(100);
+                        digitalWrite(thorlabsLEDPin, HIGH);
       delay(1000) ; 
       
-      analogWrite(pwm,70) ;       //1.1 psi
+      analogWrite(pwm,152) ;       //1.2 psi
+            digitalWrite(thorlabsLEDPin, LOW);
+            delay(100);
+                        digitalWrite(thorlabsLEDPin, HIGH);
       delay(1000) ;
   
-      analogWrite(pwm,57) ;     
+      analogWrite(pwm,127) ;     // 1 psi
+            digitalWrite(thorlabsLEDPin, LOW);
+            delay(100);
+                        digitalWrite(thorlabsLEDPin, HIGH);
       delay(1000) ; 
       
-      analogWrite(pwm,45) ; 
-      delay(1000) ;
-       
-      analogWrite(pwm,32) ;       // 0.5 psi
-      delay(1000) ;
-       
-      analogWrite(pwm,19) ; 
-      delay(1000) ; 
-  
-      analogWrite(pwm,6) ;         // 0.1 psi
-      delay(1000) ;
       }
-    else if(buttonState1 == HIGH && buttonState2 == HIGH){
+    else if(buttonState2 == HIGH){
+      digitalWrite(LEDPin, LOW);
       delay(500);
 
-      analogWrite(pwm, 64);
-      delay(2000);
+      analogWrite(pwm, 127);
+      delay(10000);
       }
-    //else if(buttonState1 == LOW){    }
+    else if(buttonState1 == LOW && buttonState2 == LOW){
+      digitalWrite(LEDPin, LOW);
+      analogWrite(pwm, 0);
+      }
 
       
 }
